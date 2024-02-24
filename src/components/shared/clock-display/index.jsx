433d658da667a06f2492661581ffc65f02ba29f9
Jisun696 <istiakkashem35@gmail.com>
2024-02-24 23:23:38 +0600

    added a conditional logic in ClockDisplay

--- src/components/shared/clock-display/index.jsx
+++ src/components/shared/clock-display/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const ClockDisplay = ({ date, title, timezone, offset }) => {
+  const offsetHour = offset / 60;
   return (
     <div>
       <h1>Title: {title}</h1>
       <h1>{date.toString()}</h1>
       <p>
-        {timezone} | {offset / 60}
+        {timezone}
+        {offsetHour < 0 ? `+${Math.abs(offsetHour)}` : `-${Math.abs(offsetHour)}`}
       </p>
     </div>
   );
