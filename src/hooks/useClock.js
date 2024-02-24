6bddea1f0d91755509d18c55795c7dc4f11cd2c5
Jisun696 <istiakkashem35@gmail.com>
2024-02-24 21:59:35 +0600

    Reafctor and edit codes in useClock hook also tested by calling it from app.jsx

--- src/hooks/useClock.js
+++ src/hooks/useClock.js
@@ -2,17 +2,6 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import { addMinutes } from 'date-fns';
 
-const init = {
-  id: '',
-  title: '',
-  timezone: {
-    type: '',
-    offset: '',
-  },
-  date_utc: null,
-  date: null,
-};
-
 const TIMEZONE_OFFSET = {
   PST: -8 * 60,
   EDT: -4 * 60,
@@ -21,36 +10,37 @@ const TIMEZONE_OFFSET = {
 };
 
 const useClock = (timezone, offset = 0) => {
-  const [state, setState] = useState({ ...init });
-  const [utc, setUtc] = useState(null);
+  const [localDate, setLocalDate] = useState(null);
+  const [localOffset, setLocalOffset] = useState(0);
+  const [utc, setUtc] = useState(null); //local state
 
   useEffect(() => {
     let d = new Date();
-    const localOffset = d.getTimezoneOffset();
-    d = addMinutes(d, localOffset);
+    const lo = d.getTimezoneOffset();
+    d = addMinutes(d, lo);
     setUtc(d);
+    setLocalOffset(lo);
   }, []);
 
   useEffect(() => {
-    if (utc !== null && timezone) {
-      offset = TIMEZONE_OFFSET[timezone] ?? offset;
-      const UTCconverted = addMinutes(utc, offset);
-      setState({
-        ...state,
-        date_utc: utc,
-        date: UTCconverted,
-      });
-    } else {
-      setState({
-        ...state,
-        date_utc: utc,
-        date: utc,
-      });
+    if (utc !== null) {
+      if (timezone) {
+        offset = TIMEZONE_OFFSET[timezone] ?? offset;
+        const UTCconverted = addMinutes(utc, offset);
+        setLocalDate(UTCconverted);
+      } else {
+        const UTCconverted = addMinutes(utc, -localOffset);
+        setLocalDate(UTCconverted);
+      }
     }
   }, [utc]);
 
   return {
-    clock: state,
+    date: localDate,
+    dateUTC: utc,
+    offset,
+    timezone,
+    localOffset,
   };
 };
 
