0161c3e0cb0b66923de526740e7d2156aaa328b5
Jisun696 <istiakkashem35@gmail.com>
2024-02-25 17:12:29 +0600

    Test with more dummy clocks
    Tested with more dummy clocks by calling useClock hook from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import ClockList from './components/clock-list';
 import LocalClock from './components/local-clock';
+import ClockDisplay from './components/shared/clock-display';
 import useClock from './hooks/useClock';
 
 function App() {
   const { date: localClock, localOffset, localTimezone } = useClock();
-  localClock && console.log(localClock);
+  const { date: india, offset, timezone } = useClock('GMT', -5.5 * 60);
+  const { date: pakistan, offset: offset2, timezone: timezone2 } = useClock('GMT', -5 * 60);
   return (
     <>
       {localClock && <LocalClock date={localClock} offset={localOffset} timezone={localTimezone} />}
       <ClockList />
+      {india && <ClockDisplay date={india} title={'India'} offset={offset} timezone={timezone} />}
+      {pakistan && (
+        <ClockDisplay date={pakistan} title={'Pakistan'} offset={offset2} timezone={timezone2} />
+      )}
     </>
   );
 }
