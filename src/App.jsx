022426b315deb1c3546fa434cf3e766776f63cc1
Jisun696 <istiakkashem35@gmail.com>
2024-02-25 19:16:59 +0600

    Remove the test dummy codes from App

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,10 @@ import useClock from './hooks/useClock';
 
 function App() {
   const { date: localClock, localOffset, localTimezone } = useClock();
-  const { date: india, offset, timezone } = useClock('GMT', -5.5 * 60);
-  const { date: pakistan, offset: offset2, timezone: timezone2 } = useClock('GMT', -5 * 60);
   return (
     <>
       {localClock && <LocalClock date={localClock} offset={localOffset} timezone={localTimezone} />}
       <ClockList />
-      {india && <ClockDisplay date={india} title={'India'} offset={offset} timezone={timezone} />}
-      {pakistan && (
-        <ClockDisplay date={pakistan} title={'Pakistan'} offset={offset2} timezone={timezone2} />
-      )}
     </>
   );
 }
